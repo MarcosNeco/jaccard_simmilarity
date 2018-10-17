5a772451ef7430ac7fed3031a2ff99928b61df67
MarcosNeco <marcosmattosjunior@gmail.com>
2018-10-17 04:10:46 -0300

    done jaccard main algorithm

--- build.sbt
+++ build.sbt
@@ -8,4 +8,5 @@ lazy val root = (project in file("."))
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.3.0"
 libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.3.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.3.0"
 libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.3.0"
--- src/main/scala/com/jacsimm/core/JaccardSimilarityAlgorithm.scala
+++ src/main/scala/com/jacsimm/core/JaccardSimilarityAlgorithm.scala
@@ -2,7 +2,10 @@ package com.jacsimm.core
 
 import com.jacsimm.configuration.Configuration
 import com.jacsimm.model.DocumentView
-import org.apache.spark.SparkConf
+import org.apache.spark.sql.{Column, SparkSession}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -13,18 +16,37 @@ object JaccardSimilarityAlgorithm{
   }
 
   def calculate(): Unit ={
-    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("streaming-kafkaviewdoc")
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
+     val sparkConf = new SparkConf().setMaster("local[*]").setAppName("streaming-kafkaviewdoc")
+     val ssc = new StreamingContext(sparkConf, Seconds(60))
 
      val message = KafkaUtils.createDirectStream[String, String](ssc, LocationStrategies.PreferConsistent,
       ConsumerStrategies.Subscribe[String, String](Configuration.topicSet, Configuration.kafkaParams))
 
-     val documentsView  = message
-       .map(record => (record.key(), record.value()))
+     val tempTables = List[String]
+     message
+       .map(record => DocumentView(record.key(), record.value()))
+       .foreachRDD(rdd=>{
+         val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
+         import spark.implicits._
+         val documentsViewDF = rdd.toDF()
 
-     val totalByDocument = documentsView
-       .groupByKey()
-       .map((document) => (document._1, document._2.size))
+         val documentsViewAndTotal = documentsViewDF
+           .withColumn("totalDocuments", count("document").over(countTotalByDocument()))
+
+         //self join to combine all relation users
+         val calculatedDf = documentsViewAndTotal.as("docViewLeft")
+           .join(documentsViewAndTotal.as("docViewRight"), $"docViewLeft.user" === $"docViewRight.user")
+           .filter($"docViewLeft.document" =!= $"docViewRight.document")
+           .groupBy(col("docViewLeft.document").as("docA"), col("docViewRight.document").as("docB"))
+           .agg(max("docViewLeft.totalDocuments").as("totalDocA"),
+                max("docViewRight.totalDocuments").as("totalDocB"),
+                count("*").as("totalInCommon"))
+           .withColumn("jaccardIndex", jaccardIndex($"totalInCommon", $"totalDocA", $"totalDocB"))
+
+         val tempTableToRdd = "jaccard_table" + rdd.id.toString
+         tempTables +: tempTableToRdd
+         calculatedDf.createOrReplaceTempView(tempTableToRdd)
+      })
 
 
      ssc.start()
@@ -32,9 +54,17 @@ object JaccardSimilarityAlgorithm{
 
   }
 
-  def generateCoefficient(): Unit ={
-
+  private val countTotalByDocument = {
+    Window.partitionBy("document").orderBy("document")
   }
+
+  val jaccardIndex = udf(jaccardIndexFunc)
+
+  private val jaccardIndexFunc = (userInCommon: Int, totalDocA: Int, totalDocB: Int) => Float {
+      val union = totalDocA + totalDocB - userInCommon
+      userInCommon / union
+  }
+
 }
 
 
