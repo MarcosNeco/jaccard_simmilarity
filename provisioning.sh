d40ec5e4dcc1e96baba482182ce298f98e00665d
Marcos Neco de Mattos Junior <marcos.mattos@neogrid.com>
2018-10-23 18:49:02 -0300

    change environment

--- provisioning.sh
+++ provisioning.sh
@@ -1,46 +1,31 @@
 #!/bin/sh
+unzip_servers(){
+  if [ ! -d zookeeper-3.4.12_server ]; 
+    then
+      unzip zookeeper-3.4.12.zip
+    fi
 
-
-
-dowload_dep(){
-}
-run_ws() {
-  echo "Starting recommendation-docs-ws"
-  (cd recommendation-docs-ws/ && exec sbt dist);
-  (cd recommendation-docs-ws/ && exec unzip target/universal/recommendation-docs-ws*.zip);
-  (cd recommendation-docs-ws/ && exec nohup ./recommendation-docs-ws-*/bin/recommendation-docs-ws -Dplay.crypto.secret="recommendationglobo" -Dhttp.port=8080 &)
-}
-
-run_pipe() {
-  echo "Starting recommendation-docs-pipe"
-  (cd recommendation-docs-pipe/ && exec nohup /opt/spark-2.0.2-bin-hadoop2.7/bin/spark-submit --class com.globo.selecao.recommendation.streaming.JaccardIndexCalculation target/scala-2.11/recommendation-docs-pipe-assembly-*.jar &)
+  if [ ! -d kafka_2.11_server ];
+    then
+      unzip kafka_2.11.zip
+    fi
 }
 
-run () {
-  run_ws
-  run_pipe
+start_servers(){
+  echo "initializing zookeeper service"
+  start ./zookeeper-3.4.12_server/bin/zkServer.sh
+  echo "initializing kafka service"
+  start .\\kafka_2.11_server\\bin\\windows\\kafka-server-start.bat \\kafka_2.11_server\\config\\server.properties
 }
 
-compile () {
-  echo "Compiling recommendation-config"
-  (cd recommendation-config/ && exec sbt clean publishLocal)
-  echo "Compiling recommendation-redis"
-  (cd recommendation-redis/ && exec sbt clean publishLocal)
-  echo "Compiling recommendation-docs-ws"
-  (cd recommendation-docs-ws/ && exec sbt clean compile)
-  echo "Compiling recommendation-docs-pipe"
-  (cd recommendation-docs-pipe/ && exec sbt clean assembly)
+run(){
+  unzip_servers
+  start_servers
 }
 
 case "$1" in
-   "compile") compile
+   "run") run
    ;;
-   "run") compile; run
-   ;;
-   "run_ws") run_ws
-   ;;
-   "run_pipe") run_pipe
-   ;;
-   *) echo "Usage[run|run_ws|run_pipe|compile]"
+   *) echo "Usage[run]"
    ;;
 esac
